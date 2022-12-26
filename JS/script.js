ff053fb3157db341dc78c2dd86d9547f7cdbc05b
Maciej Kurpiecki <recon4173@wp.pl>
2022-12-26 16:47:18 +0100

    added "add new task"

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,12 @@
             ddone: true,
         },
     ];
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
     const render = () => {
         let htmlString = "";
 
@@ -20,8 +26,20 @@
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
     const init = () => {
         render();
-    }
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+    };
     init();
 }
